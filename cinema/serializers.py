6226dfa8a79c491a8adc726b647563c5981d8e0c
ulichka <ulichka7615@gmail.com>
2025-04-23 16:29:57 +0200

    fixed

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -5,7 +5,7 @@ from cinema.models import Movie
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=True, max_length=255)
-    description = serializers.CharField(required=False)
+    description = serializers.CharField(required=False, allow_null=True)
     duration = serializers.IntegerField(min_value=1, required=True)
 
     def create(self, validated_data):
--- cinema/views.py
+++ cinema/views.py
@@ -29,6 +29,7 @@ def movie_detail(request, pk):
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         movie.delete()
